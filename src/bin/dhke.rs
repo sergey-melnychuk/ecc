600cef7070ebf7286058af94df53c66a935a1956
Sergey Melnychuk <sergey-melnychuk@users.noreply.github.com>
2024-10-03 10:35:20 +0200

    fix: ec dhke

--- src/bin/dhke.rs
+++ src/bin/dhke.rs
@@ -1,17 +1,23 @@
 use ecc::hash::hash;
 
 fn main() {
-    let a = hash(b"Alice");
+    let a = hash(b"x");
     let b = hash(b"Bob");
 
     let ec = ecc::elliptic::curves::curve_256();
-    let a = ec.mul(&ec.base, &a);
-    let b = ec.mul(&ec.base, &b);
+    let pa = ec.mul(&ec.base, &a);
+    let pb = ec.mul(&ec.base, &b);
 
-    let ab = ec.add(&b, &a);
-    let ba = ec.add(&a, &b);
+    let ab = ec.mul(&pb, &a);
+    let ba = ec.mul(&pa, &b);
+    assert_eq!(ab, ba);
 
-    println!("A+B: {:x}", ab.x);
-    println!("B+A: {:x}", ba.x);
-    println!(" ok: {}", ab == ba);
+    println!("secret A: {a:x}");
+    println!("public A: {:x}", pa.x);
+    println!("---");
+    println!("secret B: {b:x}");
+    println!("public B: {:x}", pb.x);
+    println!("---");
+    println!("shared A: {:x}", ab.x);
+    println!("shared B: {:x}", ba.x);
 }
